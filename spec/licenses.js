bc6924fcbae321e3e274f8a0f14acb11c72fa879
Michael Krotscheck <krotscheck@gmail.com>
2015-06-26 16:22:42 -0700

    Imported SPDX licenses into tree.
    Importing these licenses into the tree in order to reduce the
    number of dependencies on which this tool depends.

--- spec/licenses.js
+++ spec/licenses.js
@@ -1,21 +1,34 @@
 describe('Unit: License List', function () {
   'use strict';
 
+  var fs = require('fs');
+  var path = require('path');
+
+  // Construct a list of all license names.
+  var licenseFolder = path.join(__dirname, '..', 'lib', 'licenses');
+  var filenames = fs.readdirSync(licenseFolder);
+  var licenseNames = [];
+  filenames.forEach(function (fileName) {
+    licenseNames.push(path.basename(fileName, '.txt'));
+  });
+
+  // The library under test
   var licenses = require('../lib/licences.js');
-  var spdx = require('spdx-license-list');
 
   it('Should correctly identify SPDX licenses.', function () {
     /*eslint-disable guard-for-in */
-    for (var key in spdx) {
-      expect(licenses.isLicenseValid(key)).toBeTruthy();
+    for (var i = 0; i < licenseNames.length; i++) {
+      var name = licenseNames[i];
+      expect(licenses.isLicenseValid(name)).toBeTruthy();
     }
     /*eslint-enable guard-for-in */
   });
 
   it('Should correctly identify invalid license names.', function () {
     /*eslint-disable guard-for-in */
-    for (var key in spdx) {
-      expect(licenses.isLicenseValid('__' + key)).toBeFalsy();
+    for (var i = 0; i < licenseNames.length; i++) {
+      var name = licenseNames[i];
+      expect(licenses.isLicenseValid('__' + name)).toBeFalsy();
     }
     /*eslint-enable guard-for-in */
   });
